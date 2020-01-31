a218d92b31c3fbd17d95ca481f5f2f0f5e1d4357
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-01-31 10:16:24 -0500

    added Secrets folder

---
+++ WeatherApp/WeatherApp/Secrets/Secrets.swift
@@ -0,0 +1,19 @@
+//
+//  Secrets.swift
+//  WeatherApp
+//
+//  Created by Amy Alsaydi on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+
+struct SecretsKey {
+    static let key = "993636215e910eb8feec1499a08963a6"
+}
+
+
+
+
+// endpoint: https://api.darksky.net/forecast/\(SecretsKey.key)/37.8267,-122.4233
